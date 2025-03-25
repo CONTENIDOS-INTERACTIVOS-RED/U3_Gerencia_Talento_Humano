9e7e576e9ca6b138cc84d402b0bbbac3802d822a
Francisco Lizcano <flizcanor@icloud.com>
2025-03-24 19:47:57 -0500

    configuración inicial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,11 +22,7 @@
             .content-wrapper.px-lg-4.pt-lg-4.px-xxl-5.py-xxl-5.text-start
               p.home-main-title UNIDAD {{ `${globalData.numeroUnidad}` }}
               h1.home-title.mb-4 {{ globalData.tituloUnidad }}
-              p.descripcion.mb-4 Desde los comienzos de la humanidad, la relación entre los principios éticos, el desarrollo económico y la sostenibilidad ha sido fundamental para garantizar un progreso equilibrado y equitativo. En un contexto globalizado, donde los desafíos éticos y ambientales son cada vez más evidentes, la bioética emerge como una herramienta clave para orientar decisiones económicas responsables que respeten tanto el medio ambiente como los derechos humanos.
-
-              p.descripcion.mb-4 En esta unidad, profundizaremos en el rol de la bioética como pilar de la sostenibilidad en el desarrollo económico, explorando temas esenciales como la aplicación de principios éticos al uso de recursos naturales, la influencia de las políticas globales en la protección de los derechos humanos y la ética en la innovación tecnológica. 
-              
-              p #[b Prepárate para explorar los retos y oportunidades que la sostenibilidad y la bioética presentan en un mundo en constante evolución.]
+              p.descripcion.mb-4 El proceso de adquisición de talento abarca reclutamiento y selección, buscando incorporar personal alineado con la empresa. Se define el perfil del cargo y se activan diversas fuentes de reclutamiento. La selección incluye pruebas, entrevistas y validación de antecedentes. La compensación laboral combina salario, prestaciones y beneficios extralegales para retener talento. El desarrollo organizacional impulsa el cambio, optimiza estructuras y fortalece la cultura empresarial. La gestión del conocimiento facilita la innovación y toma de decisiones estratégicas, garantizando sostenibilidad y competitividad en el mercado.
 
               .button-container.mt-auto
                 router-link.btn-iniciar.w-100.w-md-auto(:to="{ name: 'introduccion' }")
@@ -149,12 +145,12 @@ export default {
 .iniciar-btn-container
   .iniciar-btn-text
     transition: all 0.25s ease-in-out
-    background-color: $color-acento-botones !important
+    background-color: $color-acento-contenido !important
     color: #0B0A0A !important
   .icon
     transition: all 0.25s ease-in-out
     // background-color: #083E61 !important
-    background-color: darken($color-acento-botones, 30% ) !important
+    background-color: #496406 !important
   &:hover
     .icon
       transform: scale(1.06)
